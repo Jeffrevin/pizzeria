e17632107ac4fd9ac2fdd39514f6720d0f3ae697
Kevin Zhong <jeffrevin@gmail.com>
2017-08-01 19:30:49 +0000

    better logo position, upgraded nav

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 // A $(document).ready() block.
 $(document).ready(function() {
     // All your Javascript
+    // code to cycle through carousel
+    $("#myCarousel").carousel({
+        interval: 1500
+    });
+    //code to add and delete active class
+    $("ul li").click(function(){
+    
+    var clicked_tab = $(this);
+    clicked_tab.addClass("active");
+    clicked_tab.parent().children("li").not(clicked_tab).removeClass("active");
+    });
+    //
     
 });
