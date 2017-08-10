5a27da7b725f16fc8bb339f65563079760cdb924
Kevin Zhong <jeffrevin@gmail.com>
2017-08-10 15:58:07 +0000

    divs in event pages

--- script.js
+++ script.js
@@ -29,21 +29,21 @@ $(document).ready(function() {
 
 
 
-function add(type) {
-  //Create an input type dynamically.   
-  var element = document.createElement("input");
-  //Assign different attributes to the element. 
-  element.type = type;
-  element.value = type; // Really? You want the default value to be the type string?
-  element.name = type; // And the name too?
-  element.onclick = function() { // Note this is a function
-    alert("blabla");
-  };
+// function add(type) {
+//   //Create an input type dynamically.   
+//   var element = document.createElement("input");
+//   //Assign different attributes to the element. 
+//   element.type = type;
+//   element.value = type; // Really? You want the default value to be the type string?
+//   element.name = type; // And the name too?
+//   element.onclick = function() { // Note this is a function
+//     alert("blabla");
+//   };
 
-  var foo = document.getElementById("selectItems");
-  //Append the element in page (in span).  
-  foo.appendChild(element);
-}
-document.getElementById("btnAdd").onclick = function() {
-  add("#selectItems");
-};
+//   var foo = document.getElementById("selectItems");
+//   //Append the element in page (in span).  
+//   foo.appendChild(element);
+// }
+// document.getElementById("btnAdd").onclick = function() {
+//   add("#selectItems");
+// };
